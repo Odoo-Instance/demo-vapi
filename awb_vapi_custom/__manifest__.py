35b0b5c25174908776c4e8de551ae875e7c88efe
alejandro.alain <alejandro.alain@achievewithoutborders.com>
2022-11-07 06:14:20 +0800

    [ADD] Initial Commit - project folder

---
+++ awb_vapi_custom/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'AWB Via Appia Customization',
+    'summary': "AWB Via Appia Customization",
+    'version': '1.0.0',
+    'author': "Achieve Without Borders, Inc.",
+    'website': "https://www.achievewithoutborders.com/",
+    'description': """
+ """,
+    'category': 'Customization',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
