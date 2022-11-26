6af5be8e3255cc2dabeca0ef0f002f442fec958f
alejandro.alain <alejandro.alain@achievewithoutborders.com>
2022-11-26 21:11:37 +0800

    [FIX] #42 - Changed label

--- awb_timesheet_portal/__manifest__.py
+++ awb_timesheet_portal/__manifest__.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+##############################################################################
+#
+#   ACHIEVE WITHOUT BORDERS
+#
+##############################################################################
 {
     'name': 'AWB Timesheet Portal',
-    'summary': "AWB Via Appia Customization",
     'version': '15.0.1.0.1',
     'author': "Achieve Without Borders, Inc.",
     'website': "https://www.achievewithoutborders.com/",
-    'description': """
- """,
-    'category': 'Customization',
+    'description': """ Description Text """,
+    'category': 'Timesheet',
     'depends': ['base', 'hr_timesheet', 'timesheet_grid'],
-    'data': [
-    ],
     'license': 'LGPL-3',
     'data': [
         'views/templates.xml',
     ],
     'assets': {
-         'web.assets_frontend': ['awb_timesheet_portal/static/js/timesheet_popup.js']
+        'web.assets_frontend': ['awb_timesheet_portal/static/js/timesheet_popup.js']
     },
     'installable': True,
     'application': False,
-    'auto_install': False,
+    'auto_install': False
 }
