4aa83ffa15b98b01c4da97374121c8ef5152979f
arun kaul <arun.kaul@achievewithoutborders.com>
2022-11-29 12:50:29 +0530

    [Changes-19 to 20] #[awb_timesheet_notification]: Made Some changes In code in module

--- awb_timesheet_notification/models/res_config_settings.py
+++ awb_timesheet_notification/models/res_config_settings.py
@@ -4,8 +4,9 @@ from odoo import api, fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
+    '''Employee Timesheet Reminder'''
     send_employee_reminder = fields.Boolean(
-        string="Send birthday with to employee?",
+        string="Send Employee Timesheet Reminder?",
     )
     dayofweek = fields.Selection([
         ('0', 'Monday'),
@@ -25,6 +26,28 @@ class ResConfigSettings(models.TransientModel):
         ], 'Select No. of Week')
     time = fields.Float(string='Time')
 
+    ''' Approver TImesheet Feild'''
+    send_aprrover_reminder = fields.Boolean(
+        string="Send Approver Timesheet Reminder",
+    )
+    approver_dayofweek = fields.Selection([
+        ('0', 'Monday'),
+        ('1', 'Tuesday'),
+        ('2', 'Wednesday'),
+        ('3', 'Thursday'),
+        ('4', 'Friday'),
+        ('5', 'Saturday'),
+        ('6', 'Sunday')
+        ], 'Day of Week')
+    approver_select_week = fields.Selection([
+        ('1', '1 week'),
+        ('2', '2 Week'),
+        ('3', '3 Week'),
+        ('4', '4 Week'),
+        ('5', '5 Week'),
+        ], 'Select No. of Week')
+    approver_time = fields.Float(string='Time')
+
 
     @api.model
     def get_values(self):
@@ -34,6 +57,10 @@ class ResConfigSettings(models.TransientModel):
             dayofweek = self.env['ir.config_parameter'].sudo().get_param('dayofweek'),
             select_week = self.env['ir.config_parameter'].sudo().get_param('select_week'),
             time = self.env['ir.config_parameter'].sudo().get_param('time'),
+            send_aprrover_reminder = self.env['ir.config_parameter'].sudo().get_param('send_aprrover_reminder'),
+            approver_dayofweek = self.env['ir.config_parameter'].sudo().get_param('approver_dayofweek'),
+            approver_select_week = self.env['ir.config_parameter'].sudo().get_param('approver_select_week'),
+            approver_time = self.env['ir.config_parameter'].sudo().get_param('approver_time'),
 
         )
         return res
@@ -45,6 +72,10 @@ class ResConfigSettings(models.TransientModel):
         IrConfig.set_param('dayofweek', self.dayofweek)
         IrConfig.set_param('select_week', self.select_week)
         IrConfig.set_param('time', self.time)
+        IrConfig.set_param('send_aprrover_reminder', self.send_aprrover_reminder)
+        IrConfig.set_param('approver_dayofweek', self.approver_dayofweek)
+        IrConfig.set_param('approver_select_week', self.approver_select_week)
+        IrConfig.set_param('approver_time', self.approver_time)
 
 
 
