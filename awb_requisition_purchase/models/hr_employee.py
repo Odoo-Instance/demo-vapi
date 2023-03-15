0ca9eb014b59e8bd874d81719c8bec150f504ff4
janartanan <janarthanan.dhanasekar@achievewithoutborders.com>
2023-03-15 10:30:42 +0530

    Ticket : #70
    As a business owner, I need this purchase requisition process to be
    followed

---
+++ awb_requisition_purchase/models/hr_employee.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   ACHIEVE WITHOUT BORDERS
+#
+##############################################################################
+
+from odoo import api, fields, models
+from odoo.exceptions import Warning, UserError, ValidationError
+
+
+class HREmployee(models.Model):
+    _inherit = "hr.employee"
+
+    approve_type = fields.Selection([('ceo', 'CEO'),('dm','Department Manager'),
+                                    ('om', 'Operations Manager')], string="Approve Type")
